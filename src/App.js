4bf4935f471e9c38fbd828a038485a45e6bae33b
kimyb8870 <qhd0084@gmail.com>
2022-02-07 11:04:53 +0900

    Feature: routing 3 pages dashboard, map, alarm
    dashboard -> for dashboard info
    map -> for localsense 2d mapping
    alarm -> for check alarm push and control

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import DashboardPage from './pages/DashboardPage';
+import MapPage from './pages/MapPage';
+import AlarmPage from './pages/AlarmPage';
 
 const App = () => {
-  return <div>apulse-uwb</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<DashboardPage />} />
+      <Route path="/map" element={<MapPage />} />
+      <Route path="/alarm" element={<AlarmPage />} />
+    </Routes>
+  );
 };
 
 export default App;
