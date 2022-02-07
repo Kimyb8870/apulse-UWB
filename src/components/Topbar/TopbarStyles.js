1fc48b37debde53aa3e2e86b48e4aadf20db4fe4
kimyb8870 <qhd0084@gmail.com>
2022-02-07 16:36:46 +0900

    Feature: styling topbar with material-ui

--- src/components/Topbar/TopbarStyles.js
+++ src/components/Topbar/TopbarStyles.js
@@ -1,5 +1,27 @@
 import styled from 'styled-components';
+import { bluePalette, grayPalette } from '../../lib/palette';
 
 export const StyledTopbar = styled.div`
-  background-color: gray;
+  background-color: ${grayPalette[0]};
+  box-shadow: 0px 0px 2px 2px rgba(0, 0, 0, 0.05);
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  padding-left: 30px;
+  padding-right: 30px;
+`;
+
+export const StyledTitle = styled.h1`
+  color: ${bluePalette[6]};
+  font-weight: 700;
+  letter-spacing: 1.5px;
+`;
+
+export const FlexRow = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
--- src/components/Topbar/index.js
+++ src/components/Topbar/index.js
@@ -1,8 +1,34 @@
+import { Avatar, Badge, IconButton, Tooltip } from '@mui/material';
 import React from 'react';
-import { StyledTopbar } from './TopbarStyles';
+import { StyledTopbar, StyledTitle, FlexRow } from './TopbarStyles';
+import { Bell as BellIcon } from '../../assets/icons/bell';
+import { UserCircle as UserCircleIcon } from '../../assets/icons/user-circle';
 
 const Topbar = () => {
-  return <StyledTopbar>topbar</StyledTopbar>;
+  return (
+    <StyledTopbar>
+      <StyledTitle>Apulsetech-UWB</StyledTitle>
+      <FlexRow>
+        <Tooltip title="Notifications">
+          <IconButton sx={{ mr: 2 }}>
+            <Badge badgeContent={4} color="primary" variant="primary">
+              <BellIcon fontSize="medium" />
+            </Badge>
+          </IconButton>
+        </Tooltip>
+        <Avatar
+          sx={{
+            height: 30,
+            width: 30,
+            ml: 1,
+          }}
+          src="/static/images/avatars/avatar_1.png"
+        >
+          <UserCircleIcon fontSize="small" />
+        </Avatar>
+      </FlexRow>
+    </StyledTopbar>
+  );
 };
 
 export default Topbar;
